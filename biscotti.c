054915371fcf24c85a74c907b8a47ea1f9e1cbc6
pierre <pierre.vo@gmail.com>
2017-08-26 21:06:28 +0200

    biscotti now compiles with avr-gcc 7.1.0. Had to remove inlines definitions for that

--- biscotti.c
+++ biscotti.c
@@ -42,7 +42,7 @@
  *   Same for off-time capacitor values.  Measure, don't guess.
  */
 // Choose your MCU here, or in the build script
-//#define ATTINY 13
+#define ATTINY 13
 //#define ATTINY 25
 // FIXME: make 1-channel vs 2-channel power a single #define option
 //#define FET_7135_LAYOUT  // specify an I/O pin layout
@@ -257,7 +257,7 @@ void save_state() {  // central method for writing complete state
 }
 
 #ifndef USE_FIRSTBOOT
-inline void reset_state() {
+void reset_state() {
     mode_idx = 0;
     modegroup = 0;
     mode_override = 0;
@@ -323,7 +323,7 @@ void restore_state() {
 #endif
 }
 
-inline void next_mode() {
+void next_mode() {
     mode_idx += 1;
     if (mode_idx >= solid_modes) {
         // Wrap around, skipping the hidden modes
@@ -334,7 +334,7 @@ inline void next_mode() {
 }
 
 #ifdef OFFTIM3
-inline void prev_mode() {
+void prev_mode() {
     // simple mode has no reverse
     //if (muggle_mode) { return next_mode(); }
 
@@ -434,9 +434,9 @@ void count_modes() {
 }
 
 #ifdef ALT_PWM_LVL
-inline void set_output(uint8_t pwm1, uint8_t pwm2) {
+void set_output(uint8_t pwm1, uint8_t pwm2) {
 #else
-inline void set_output(uint8_t pwm1) {
+void set_output(uint8_t pwm1) {
 #endif
     /* This is no longer needed since we always use PHASE mode.
     // Need PHASE to properly turn off the light
@@ -507,7 +507,7 @@ void blink(uint8_t val, uint8_t speed)
 }
 
 #ifdef ANY_STROBE
-inline void strobe(uint8_t ontime, uint8_t offtime) {
+void strobe(uint8_t ontime, uint8_t offtime) {
     uint8_t i;
     for(i=0;i<8;i++) {
         set_level(RAMP_SIZE);
@@ -519,7 +519,7 @@ inline void strobe(uint8_t ontime, uint8_t offtime) {
 #endif
 
 #ifdef SOS
-inline void SOS_mode() {
+void SOS_mode() {
 #define SOS_SPEED (200/4)
     blink(3, SOS_SPEED);
     _delay_4ms(SOS_SPEED*5);
@@ -572,7 +572,7 @@ uint8_t get_temperature() {
 #endif  // TEMPERATURE_MON
 
 #ifdef OFFTIM3
-inline uint8_t read_otc() {
+uint8_t read_otc() {
     // Read and return the off-time cap value
     // Start up ADC for capacitor pin
     // disable digital input on ADC pin to reduce power consumption
--- tk-attiny.h
+++ tk-attiny.h
@@ -22,7 +22,7 @@
  */
 
 // Choose your MCU here, or in the main .c file, or in the build script
-//#define ATTINY 13
+#define ATTINY 13
 //#define ATTINY 25
 
 /******************** hardware-specific values **************************/
