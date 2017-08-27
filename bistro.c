6317da165d5cb1340e7231a5b55dbedef9cff726
Torreip <pierre.vo@gmail.com>
2017-08-27 11:13:09 +0200

    Trying to split the file to make it more readable, not sure it's a good idea...

--- bistro.c
+++ bistro.c
@@ -48,85 +48,8 @@
  *
  *   Same for off-time capacitor values.  Measure, don't guess.
  */
-// Choose your MCU here, or in the build script
-//#define ATTINY 13
-//#define ATTINY 25
-// FIXME: make 1-channel vs 2-channel power a single #define option
-#define FET_7135_LAYOUT  // specify an I/O pin layout
-// Also, assign I/O pins in this file:
-#include "tk-attiny.h"
 
-/*
- * =========================================================================
- * Settings to modify per driver
- */
-
-// FIXME: make 1-channel vs 2-channel power a single #define option
-//#define FAST 0x23           // fast PWM channel 1 only
-//#define PHASE 0x21          // phase-correct PWM channel 1 only
-#define FAST 0xA3           // fast PWM both channels
-#define PHASE 0xA1          // phase-correct PWM both channels
-
-#define VOLTAGE_MON         // Comment out to disable LVP
-
-#define OFFTIM3             // Use short/med/long off-time presses
-                            // instead of just short/long
-
-// ../../bin/level_calc.py 64 1 10 1300 y 3 0.23 140
-#define RAMP_SIZE  64
-// log curve
-//#define RAMP_7135  3,3,3,3,3,3,4,4,4,4,4,5,5,5,6,6,7,7,8,9,10,11,12,13,15,16,18,21,23,27,30,34,39,44,50,57,65,74,85,97,111,127,145,166,190,217,248,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,0
-//#define RAMP_FET   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,6,11,17,23,30,39,48,59,72,86,103,121,143,168,197,255
-// x**2 curve
-//#define RAMP_7135  3,5,8,12,17,24,32,41,51,63,75,90,105,121,139,158,178,200,223,247,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,0
-//#define RAMP_FET   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,4,6,9,12,16,19,22,26,30,33,37,41,45,50,54,59,63,68,73,78,84,89,94,100,106,111,117,123,130,136,142,149,156,162,169,176,184,191,198,206,214,221,255
-// x**3 curve
-#define RAMP_7135  3,3,4,5,6,8,10,12,15,19,23,28,33,40,47,55,63,73,84,95,108,122,137,153,171,190,210,232,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,0
-#define RAMP_FET   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,5,8,11,14,18,22,26,30,34,39,44,49,54,59,65,71,77,84,91,98,105,113,121,129,137,146,155,164,174,184,194,205,216,255
-// x**5 curve
-//#define RAMP_7135  3,3,3,4,4,5,5,6,7,8,10,11,13,15,18,21,24,28,33,38,44,50,57,66,75,85,96,108,122,137,154,172,192,213,237,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,0
-//#define RAMP_FET   0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,3,6,9,13,17,21,25,30,35,41,47,53,60,67,75,83,91,101,111,121,132,144,156,169,183,198,213,255
-
-// uncomment to ramp up/down to a mode instead of jumping directly
-#define SOFT_START
-
-// Enable battery indicator mode?
-#define USE_BATTCHECK
-// Choose a battery indicator style
-//#define BATTCHECK_4bars  // up to 4 blinks
-//#define BATTCHECK_8bars  // up to 8 blinks
-#define BATTCHECK_VpT  // Volts + tenths
-
-// output to use for blinks on battery check (and other modes)
-#define BLINK_BRIGHTNESS    RAMP_SIZE/4
-// ms per normal-speed blink
-#define BLINK_SPEED         500
-
-// Hidden modes are *before* the lowest (moon) mode, and should be specified
-// in reverse order.  So, to go backward from moon to turbo to strobe to
-// battcheck, use BATTCHECK,STROBE,TURBO .
-#define HIDDENMODES         BIKING_STROBE,BATTCHECK,POLICE_STROBE,TURBO
-
-#define TURBO     RAMP_SIZE       // Convenience code for turbo mode
-#define BATTCHECK 254       // Convenience code for battery check mode
-#define GROUP_SELECT_MODE 253
-#define TEMP_CAL_MODE 252
-// Uncomment to enable tactical strobe mode
-//#define STROBE    251       // Convenience code for strobe mode
-// Uncomment to unable a 2-level stutter beacon instead of a tactical strobe
-#define BIKING_STROBE 250   // Convenience code for biking strobe mode
-// comment out to use minimal version instead (smaller)
-#define FULL_BIKING_STROBE
-//#define RAMP 249       // ramp test mode for tweaking ramp shape
-#define POLICE_STROBE 248
-//#define RANDOM_STROBE 247
-//#define SOS 246
-
-// thermal step-down
-#define TEMPERATURE_MON
-
-// Calibrate voltage and OTC in this file:
-#include "tk-calibration.h"
+#include "driver_config_bistro.h"
 
 /*
  * =========================================================================
